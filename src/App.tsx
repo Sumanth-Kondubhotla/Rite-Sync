f2786a62552fe39cadb15d68ab5f224b6d00d204
Sarvesh <sarvesh@aanvy.com>
2023-12-17 23:48:26 +0530

    Connections Page UI changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import { ThemeProvider } from "@mui/system";
 import "./App.css";
 import customTheme from "./theme/custom-theme";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Root from "./pages/Root";
 import Dashboard from "./pages/Dashboard";
+import Connections from "./pages/Connections";
 
 const router = createBrowserRouter([
   {
@@ -23,14 +21,17 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
-    children: [{path: 'dashboard', element: <Dashboard />}]
+    children: [
+      { path: "dashboard", element: <Dashboard /> },
+      { path: "connections", element: <Connections /> },
+    ],
   },
 ]);
 
 const App = () => {
   return (
     <ThemeProvider theme={customTheme}>
-       <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 };
