a55b6879a44f395662f46b5528ce1e439f4f68e5
Sumanth Kondubhotla <sumanth503@gmail.com>
2023-12-18 20:36:51 +0530

    feat instances

---
+++ src/Icons/DashboardIcon.tsx
@@ -0,0 +1,30 @@
+const DashboardIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+    >
+      <mask
+        id="mask0_579_1432"
+        maskUnits="userSpaceOnUse"
+        x="0"
+        y="0"
+        width="16"
+        height="16"
+      >
+        <rect width="16" height="16" fill="#D9D9D9" />
+      </mask>
+      <g mask="url(#mask0_579_1432)">
+        <path
+          d="M2 7.33333V2H7.33333V7.33333H2ZM2 14V8.66667H7.33333V14H2ZM8.66667 7.33333V2H14V7.33333H8.66667ZM8.66667 14V8.66667H14V14H8.66667ZM3.33333 6H6V3.33333H3.33333V6ZM10 6H12.6667V3.33333H10V6ZM10 12.6667H12.6667V10H10V12.6667ZM3.33333 12.6667H6V10H3.33333V12.6667Z"
+          fill="#2B2B2B"
+        />
+      </g>
+    </svg>
+  );
+};
+
+export default DashboardIcon;
