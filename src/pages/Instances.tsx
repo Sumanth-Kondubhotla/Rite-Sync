169fd9ccfd3b0c8a6fa8c0aaa3ce5cb16e086295
Sarvesh <sarvesh@aanvy.com>
2023-12-20 12:20:55 +0530

    Removed commented code

--- src/pages/Instances.tsx
+++ src/pages/Instances.tsx
@@ -20,12 +20,6 @@ interface InstancesState {
   targetInstance: string;
 }
 
-// const choices = [
-//   { label: "Option-1", value: "1" },
-//   { label: "Option-2", value: "2" },
-//   { label: "Option-3", value: "3" },
-// ];
-
 const StyledHeader = styled("h2")(() => ({
   color: "#272727",
   fontSize: "14px",
